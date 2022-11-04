95f45f443652794c4382c06a858485d2dc583509
Karimova Dina <dinaidina@mail.ru>
2022-11-05 02:46:36 +0300

    initial commit in testng branch

--- src/test/java/ru/netology/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,7 @@
 package ru.netology.ru.netology.service;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 class CashbackHackServiceTest {
 
@@ -13,7 +11,7 @@ class CashbackHackServiceTest {
 
         int expected = 100;
         int actual = service.remain(900);
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
@@ -22,21 +20,24 @@ class CashbackHackServiceTest {
 
         int expected = 990;
         int actual = service.remain(10);
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
+
     @Test
     void noNeedToIncreaseThePurchaseIfAmountIs1000() {
         CashbackHackService service = new CashbackHackService();
 
         int expected = 0;
         int actual = service.remain(1000);
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
     }
+
     @Test
     void noNeedToIncreaseThePurchaseIfAmountIsMore1000() {
         CashbackHackService service = new CashbackHackService();
 
         int expected = 0;
         int actual = service.remain(2000);
-        Assertions.assertEquals(expected, actual);
+        Assert.assertEquals(actual, expected);
+    }
 }
